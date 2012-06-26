a9f7452875c5568ed993dbdb21314e737b65d679
Kyle Isom <kyle@tyrfingr.is>
2012-06-26 07:43:09 -0400

    add mail-server function

--- src/simple_email/core.clj
+++ src/simple_email/core.clj
@@ -1,1 +1,19 @@
-(ns simple-email.core)
+(ns simple-email.core
+  (:import [org.apache.commons.email SimpleEmail]))
+
+(defn mail-server
+  [mail-host mail-port mail-ssl mail-user mail-pass mail-from]
+  (fn [recipients subject message]
+    (doto (SimpleEmail.)
+      (.setHostName mail-host)
+      (if mail-ssl
+        (.setSslSmtpPort (Integer. mail-port))
+        (.setSmtpPort (Integer. mail-port)))
+      (.setSSL mail-ssl)
+      (doseq [recipient recipients]
+        (.addTo recipient))
+      (.setFrom mail-from)
+      (.setSubject subject)
+      (.setMsg message)
+      (.setAuthentication mail-user mail-pass)
+      (.send))))
