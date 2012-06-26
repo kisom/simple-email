d0a4dc0feb584934781a7faef81d5a2739181a3d
Kyle Isom <kyle@tyrfingr.is>
2012-06-26 18:13:25 -0400

    bump version number

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject simple-email "0.1.0-DEV"
+(defproject simple-email "0.3.0-DEV"
   :description "Clojure wrapper around Apache Commons Email"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.apache.commons/commons-email "1.2"]])
