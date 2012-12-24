1ed41780071b4d45cfa5846781760bb6fa4cfb76
ken restivo <ken@restivo.org>
2012-12-23 22:55:58 -0800

    1.3 is fine, 1.4 isn't actually necessary.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject simple-email "1.0.7"
   :description "Clojure wrapper around Apache Commons Email"
-  :dependencies [[org.clojure/clojure "1.4.0"]
+  :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.apache.commons/commons-email "1.2"]]
   :aot [simple-email.core]
   :url "https://github.com/kisom/simple-email"
