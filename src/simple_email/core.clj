191518838e676c20c024686d3b3faaddc6ce2b2f
ken restivo <ken@restivo.org>
2012-12-23 22:12:12 -0800

    First attempt at demacroifying

--- src/simple_email/core.clj
+++ src/simple_email/core.clj
@@ -18,56 +18,56 @@
           (= bool-string "1") true
           true false)))
 
-(defmacro send-message
+(defn send-message
   [server recipients subject message]
-  `(try
-     (~server ~recipients ~subject ~message)
-     (catch Exception e#
+  (try
+     (server recipients subject message)
+     (catch Exception e
        (do
-         (let [cause# (if-not (nil? (.getCause e#))
-                        (.toString (.getCause e#))
+         (let [cause (if-not (nil? (.getCause e))
+                        (.toString (.getCause e))
                         nil)]
            (hash-map :ok false
-                     :type (.getClass e#)
-                     :message (.getMessage e#)
-                     :cause cause#))))))
+                     :type (.getClass e)
+                     :message (.getMessage e)
+                     :cause cause))))))
 
-(defmacro mail-server
+(defn mail-server
   [mail-host mail-port mail-ssl mail-user mail-pass mail-from]
-  `(fn [recipients# subject# message#]
-       (let [email# (SimpleEmail.)
-             mail-port# (cond (string? ~mail-port) ~mail-port
-                              (number? ~mail-port) (str ~mail-port)
-                              :else ~mail-port)
-             mail-from-name# (re-find #"^[^<]+" ~mail-from)
-             mail-from-name# (if-not (nil? mail-from-name#)
-                               (clojure.string/replace mail-from-name#
+  (fn [recipients subject message]
+       (let [email (SimpleEmail.)
+             mail-port (cond (string? mail-port) mail-port
+                              (number? mail-port) (str mail-port)
+                              :else mail-port)
+             mail-from-name (re-find #"^[^<]+" mail-from)
+             mail-from-name (if-not (nil? mail-from-name)
+                               (clojure.string/replace mail-from-name
                                                        #"\s*$" "")
                                nil)
-             mail-from-addr# (re-find #"<.+>" ~mail-from )
-             mail-from-addr# (if-not (nil? mail-from-addr#)
-                               (clojure.string/replace mail-from-addr#
+             mail-from-addr (re-find #"<.+>" mail-from )
+             mail-from-addr (if-not (nil? mail-from-addr)
+                               (clojure.string/replace mail-from-addr
                                                     #"<(.+)>" "$1")
                                nil)]
          (do
-           (.setHostName email# ~mail-host)
-           (.setSslSmtpPort email# mail-port#)
-           (.setSmtpPort email# (Integer. mail-port#))
-           (.setTLS email# ~mail-ssl)
-           (doseq [recipient# recipients#]
-             (.addTo email# recipient#))
-           (cond (and (nil? mail-from-addr#)
-                      (nil? mail-from-name#)) (.setFrom email#
+           (.setHostName email mail-host)
+           (.setSslSmtpPort email mail-port)
+           (.setSmtpPort email (Integer. mail-port))
+           (.setTLS email mail-ssl)
+           (doseq [recipient recipients]
+             (.addTo email recipient))
+           (cond (and (nil? mail-from-addr)
+                      (nil? mail-from-name)) (.setFrom email
                                                         (format "%s@%s"
-                                                                ~mail-user
-                                                                ~mail-host))
-                      (nil? mail-from-addr#) (.setFrom email# mail-from-name#)
-                      (nil? mail-from-name#) (.setFrom email# mail-from-addr#)
-                      :else (.setFrom email# mail-from-addr# mail-from-name#))
-           (.setSubject email# subject#)
-           (.setMsg email# message#)
-           (.setAuthentication email# ~mail-user ~mail-pass)
-           (.send email#)))))
+                                                                mail-user
+                                                                mail-host))
+                      (nil? mail-from-addr) (.setFrom email mail-from-name)
+                      (nil? mail-from-name) (.setFrom email mail-from-addr)
+                      :else (.setFrom email mail-from-addr mail-from-name))
+           (.setSubject email subject)
+           (.setMsg email message)
+           (.setAuthentication email mail-user mail-pass)
+           (.send email)))))
 
 (defn mail-server-from-env
   "Set up a mail server with environment variables."
